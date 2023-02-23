4d362db7bca14bbeaa56fd9a7044d9a43e57c8f3
Marlon Yepes <myepes82@misena.edu.co>
2023-02-22 20:52:28 -0500

    [2023-02-22] New microservises were added, docker compose file added and tested

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dh202302-g1
+
+## INTRUCTIONS
+
+-- FOR RUNNING DOCKER COMPOSE
+
+# 1 ) open git bash terminal in folder an run the following command
+    - docker compose up -d
--- api-wallet/src/main/java/com/dh/g1/apiwallet/ApiWalletApplication.java
+++ api-wallet/src/main/java/com/dh/g1/apiwallet/ApiWalletApplication.java
@@ -1,4 +1,4 @@
-package com.dh.g1;
+package com.dh.g1.apiwallet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ApiCustomerApplication {
+public class ApiWalletApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiCustomerApplication.class, args);
+		SpringApplication.run(ApiWalletApplication.class, args);
 	}
 
 }
