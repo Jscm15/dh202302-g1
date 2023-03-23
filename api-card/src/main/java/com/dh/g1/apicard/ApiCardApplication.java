04008ba2472f56710dab664aab4a691c7341eb29
bedulem <santochibelen@gmail.com>
2023-03-22 21:24:51 -0300

    add api-card

--- api-card/src/main/java/com/dh/g1/apicard/ApiCardApplication.java
+++ api-card/src/main/java/com/dh/g1/apicard/ApiCardApplication.java
@@ -2,10 +2,12 @@ package com.dh.g1.apicard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableFeignClients
 public class ApiCardApplication {
 
 	public static void main(String[] args) {
--- api-card/src/main/java/com/dh/g1/apicard/controller/TarjetaCreditoController.java
+++ api-card/src/main/java/com/dh/g1/apicard/controller/TarjetaCreditoController.java
@@ -1,43 +1,32 @@
 package com.dh.g1.apicard.controller;
 
 import com.dh.g1.apicard.model.TarjetaCredito;
-import com.dh.g1.apicard.repository.ITarjetaCreditoRepository;
+import com.dh.g1.apicard.service.TarjetaCreditoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/vi/apiCard/tarjeta")
 public class TarjetaCreditoController {
 
-    private final ITarjetaCreditoRepository tarjetaCreditoRepository;
+    private final TarjetaCreditoService tarjetaCreditoService;
 
-    public TarjetaCreditoController(ITarjetaCreditoRepository tarjetaCreditoRepository) {
-        this.tarjetaCreditoRepository = tarjetaCreditoRepository;
+    public TarjetaCreditoController(TarjetaCreditoService tarjetaCreditoService) {
+        this.tarjetaCreditoService = tarjetaCreditoService;
     }
+
 
     @PostMapping()
     public void save() {
-        tarjetaCreditoRepository.save(
-                TarjetaCredito.builder()
-                        .id(UUID.randomUUID().toString())
-                        .numeroTarjeta("XYZ123")
-                        .tipoDocumento("DU")
-                        .numeroDocumento("ABC123")
-                        .moneda("USD")
-                        .limiteCalificado(new BigDecimal(500000))
-                        .limiteConsumido(new BigDecimal(100000))
-                        .limiteDisponible(new BigDecimal(400000))
-                        .build()
-        );
+        tarjetaCreditoService.save();
     }
-        @GetMapping()
+
+    @GetMapping()
         public List<TarjetaCredito> getAll(){
-            return tarjetaCreditoRepository.findAll();
+            return tarjetaCreditoService.getAll();
         }
 }
